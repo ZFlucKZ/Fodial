68942157f5eeed47e2dc9b227febc8badfa34f83
Fluke <guyguy5050@gmail.com>
2022-09-11 03:01:42 +0700

    Set up PassportJS

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,46 @@
+//* Read Docs. https://www.passportjs.org/packages/passport-local/
+
+const { model } = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+const User = require('../models/user');
+
+passport.use(
+  new LocalStrategy({ usernameField: 'email' }, function (
+    email,
+    password,
+    done
+  ) {
+    //* find a user and establish the identity
+    User.findOne({ email: email }, function (err, user) {
+      if (err) {
+        console.log('Error in finding use --> Passport');
+        return done(err);
+      }
+      if (!user || user.password != password) {
+        console.log('Invalid Username/Password');
+        return done(null, false);
+      }
+      return done(null, user);
+    });
+  })
+);
+
+//* serializing the user to decide which key is to be kept in the cookies
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+//* deserializing the user from the key in the cookies
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    if (err) {
+      console.log('Error in finding User --> Passport');
+      return done(err);
+    }
+    return done(null, user);
+  });
+});
+
+module.exports = passport;
