87921a1116e214c066f3bc76609ebd9a06ff6bc1
Fluke <guyguy5050@gmail.com>
2022-09-10 09:07:43 +0700

    Connecting to Database :: MongoDB

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/fodial_app');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error connecting to MongoDB'));
+
+db.once('open', function () {
+  console.log('Connected to Database');
+});
+
+module.exports = db;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const port = 8000;
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
 //* Set up express Layouts *//
 app.use(expressLayouts);
