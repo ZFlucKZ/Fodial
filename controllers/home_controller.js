fbb95b0c89c2896a711e20e6ea10f65570921a11
Fluke <guyguy5050@gmail.com>
2022-09-12 05:23:29 +0700

    Created Display Posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,14 @@
+const Post = require('../models/post');
+
 module.exports.home = function (req, res) {
-  return res.render('home', { title: 'Fodial | Home' });
+  // Post.find({}, function (err, posts) {
+  //   return res.render('home', { title: 'Fodial | Home', posts: posts });
+  // });
+
+  //* populate the user of each post
+  Post.find({})
+    .populate('user')
+    .exec(function (err, posts) {
+      return res.render('home', { title: 'Fodial | Home', posts: posts });
+    });
 };
