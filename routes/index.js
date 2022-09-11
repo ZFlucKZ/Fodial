1a1a9c9ba0f057deff3e8d981b0f2f85acc99afc
Fluke <guyguy5050@gmail.com>
2022-09-12 04:50:07 +0700

    Created Post method

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ const homeController = require('../controllers/home_controller');
 
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
+router.use('posts', require('./posts'));
+
+//* for any further routes, access from here
+//* router.use('routerFile', require('./routerFile'));
 
 module.exports = router;
