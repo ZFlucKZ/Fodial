b3230555dc732856b6c00e698eda9207df4c4430
Fluke <guyguy5050@gmail.com>
2022-09-12 21:44:47 +0700

    Display Comment and Fixed error and bug

---
+++ controllers/comments_controller.js
@@ -0,0 +1,37 @@
+const Comment = require('../models/comment');
+const Post = require('../models/post');
+
+module.exports.create = function (req, res) {
+  // console.log('Post ID : ' + id);
+  const postID = req.body.post.trim();
+
+  Post.findById(postID, function (err, post) {
+    console.log('Post : ' + post);
+    if (err) {
+      console.log('Error in find a post on creating a comment');
+      return;
+    }
+
+    if (post) {
+      Comment.create(
+        {
+          content: req.body.content,
+          user: req.user._id,
+          post: postID,
+        },
+        function (err, comment) {
+          //* handle error
+          if (err) {
+            console.log('Error in creating a comment');
+            return;
+          }
+
+          console.log('Comment Created');
+          post.comments.push(comment);
+          post.save();
+          return res.redirect('back');
+        }
+      );
+    }
+  });
+};
