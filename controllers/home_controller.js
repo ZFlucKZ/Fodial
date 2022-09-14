905e6003ab6005548404fe979992f64e40a39fca
Fluke <guyguy5050@gmail.com>
2022-09-14 13:26:50 +0700

    Added User Profile Link (friends)

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
 module.exports.home = function (req, res) {
   // Post.find({}, function (err, posts) {
@@ -15,6 +16,13 @@ module.exports.home = function (req, res) {
       },
     })
     .exec(function (err, posts) {
-      return res.render('home', { title: 'Fodial | Home', posts: posts });
+      User.find({}, function (err, users) {
+        // console.log(users);
+        return res.render('home', {
+          title: 'Fodial | Home',
+          posts: posts,
+          all_users: users,
+        });
+      });
     });
 };
